602e4ac7dff6329f8d218b457314c4b9566d8dfa
PhuongNgan2304 <phuongngan.nguyen2304@gmail.com>
2025-01-15 23:10:18 +0700

    Complete SplashScreen.

--- Frontend_User_DrinkUp/src/navigators/AppNavigator.tsx
+++ Frontend_User_DrinkUp/src/navigators/AppNavigator.tsx
@@ -4,8 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
+import SplashScreen from '../screens/SplashScreen';
 
 export type RootStackParamList = {
+  SplashScreen: undefined;
   Login: undefined;
   Register: undefined;
   ForgotPassword: undefined;
@@ -16,8 +18,11 @@ const Stack = createStackNavigator<RootStackParamList>();
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator  initialRouteName="SplashScreen">
+        <Stack.Screen options={{ headerShown: false}} name="SplashScreen" component={SplashScreen} />
+
+        {/* Tạm thời để headerShown của Login là false */}
+        <Stack.Screen options={{ headerShown: false}} name="Login" component={LoginScreen} /> 
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       </Stack.Navigator>
---
+++ Frontend_User_DrinkUp/src/screens/SplashScreen.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import { ActivityIndicator, Image, ImageBackground } from 'react-native';
+import { appInfo } from '../constants/appInfos';
+import { appColors } from '../constants/appColors';
+import { SpaceComponent } from '../components';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from '../navigators/AppNavigator'; 
+
+const SplashScreen = () => {
+  const navigation  = useNavigation<NavigationProp<RootStackParamList>>();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('Login'); // Chuyển đến màn hình Login
+    }, 1500); // 1.5 giây
+
+    return () => clearTimeout(timer); // Xóa timer khi component unmount
+  }, [navigation]);
+
+  return (
+    <ImageBackground
+      source = {require('../assets/images/splash-background.png')}
+      style = {{
+        flex: 1,
+          justifyContent: 'center',
+            alignItems: 'center',
+      }}
+      imageStyle = {{ flex: 1 }}>
+      <Image
+        source={require('../assets/images/logo-drinkup.png')}
+        style={{
+        width: appInfo.sizes.WIDTH *0.7,
+        resizeMode: 'contain',
+        }}
+      />
+      <SpaceComponent height={16} />
+    <ActivityIndicator color={appColors.gray} size={22} />
+  </ImageBackground >
+  );
+};
+export default SplashScreen;
