1eb8ccc495a9a01b070b743003240e4a1d223edf
KOS <ngongocthong1211@gmail.com>
2025-02-10 15:30:35 +0700

    Set up routing for user authentication and profile management

---
+++ Backend_DrinkUp/src/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { authenticate } = require('../middlewares/authMiddleware');
+const userController = require('../controllers/userController');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+router.put('/update-email', authenticate, userController.updateEmail);
+router.put('/update-phone', authenticate, userController.updatePhone);
+router.put('/update-profile', authenticate, userController.updateProfile);
+router.put('/update-profile-image', authenticate, upload.single('image'), userController.updateProfileImage);
+
+module.exports = router;
