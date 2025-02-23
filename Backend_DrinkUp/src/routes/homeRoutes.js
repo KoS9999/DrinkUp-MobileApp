e7d9ad9d574cf0d10e99579a7cf58df00c4b6672
KOS <ngongocthong1211@gmail.com>
2025-02-23 14:24:15 +0700

    Add route for product search API

--- Backend_DrinkUp/src/routes/homeRoutes.js
+++ Backend_DrinkUp/src/routes/homeRoutes.js
@@ -5,5 +5,6 @@ const homeController = require('../controllers/homeController');
 router.get('/categories', homeController.getAllCategories);
 router.get('/products/top-selling', homeController.getTopSellingProducts);
 router.get('/products/by-category', homeController.getProductsByCategory);
+router.get('/search-products', homeController.searchProducts);
 
 module.exports = router;
