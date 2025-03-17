bea0dbad0b6fd3dbae82b405a37eca6bcac1b15e
KOS <ngongocthong1211@gmail.com>
2025-03-17 15:09:05 +0700

    update router

--- Backend_DrinkUp/src/routes/productRoutes.js
+++ Backend_DrinkUp/src/routes/productRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const productController = require ('../controllers/productController');
 
 router.get('/get-product/:id', productController.findProductById);
+router.get('/product/:id/customers', productController.countCustomersByProductId);
+router.get('/product/:id/reviews', productController.getReviewsByProductId);
 
 module.exports = router;
