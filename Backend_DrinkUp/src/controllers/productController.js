81b978d8047cd89fe3667ced5d6dec2e8c4c0ab2
KOS <ngongocthong1211@gmail.com>
2025-03-17 15:05:50 +0700

    Added APIs countCustomersByProductId, getReviewsByProductId

--- Backend_DrinkUp/src/controllers/productController.js
+++ Backend_DrinkUp/src/controllers/productController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Review = require('../models/Review');
+const OrderDetail = require('../models/OrderDetail');
 const Product = require('../models/Product');
 const Topping = require('../models/Topping');
 
@@ -25,6 +27,49 @@ const findProductById = async (req, res) => {
             sweetLevels: ['Không ngọt', 'Ít ngọt', 'Ngọt bình thường', 'Nhiều ngọt'],
             iceLevels: ['Không đá', 'Ít đá', 'Đá bình thường', 'Đá riêng'], });
 
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+
+exports.countCustomersByProductId = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'ProductID không hợp lệ' });
+        }
+
+        const orderDetails = await OrderDetail.find({ product: id }).distinct('orderId');
+
+        const customerCount = orderDetails.length;
+
+        res.status(200).json({
+            success: true,
+            customerCount,
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+};
+exports.getReviewsByProductId = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: 'ProductID không hợp lệ' });
+        }
+
+        const reviews = await Review.find({ product: id }).populate('user', 'name').sort({ createdAt: -1 });
+
+        if (reviews.length === 0) {
+            return res.status(404).json({ message: 'Chưa có bình luận cho sản phẩm này' });
+        }
+
+        res.status(200).json({
+            success: true,
+            reviews,
+        });
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
