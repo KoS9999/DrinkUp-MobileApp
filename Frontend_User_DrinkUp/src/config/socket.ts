18265bc8099efa00bd491fab972fa29a58fd2121
KOS <ngongocthong1211@gmail.com>
2025-04-09 21:39:32 +0700

    Configure socket connection with websocket transport and reconnection options

---
+++ Frontend_User_DrinkUp/src/config/socket.ts
@@ -0,0 +1,10 @@
+import { io } from 'socket.io-client';
+
+const socket = io('http://192.168.2.11:5000', {
+  transports: ['websocket'],
+  forceNew: true,
+  reconnection: true,
+  timeout: 10000,
+});
+
+export default socket;
