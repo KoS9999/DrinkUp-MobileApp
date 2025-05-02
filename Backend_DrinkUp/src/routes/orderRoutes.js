e6f05965dab9422330f2df5f36124ccb0344bb50
KOS <ngongocthong1211@gmail.com>
2025-05-02 15:38:38 +0700

    update

--- Backend_DrinkUp/src/routes/orderRoutes.js
+++ Backend_DrinkUp/src/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate, authorize } = require('../middlewares/authMiddleware');
+const { authenticate } = require('../middlewares/authMiddleware');
 const orderController = require('../controllers/orderController');
 
 router.post('/create/cod', authenticate, orderController.createOrder);
@@ -8,5 +8,4 @@ router.get('/branches', orderController.getBranches);
 router.post('/apply-coupon', authenticate, orderController.applyCoupon);
 router.post('/redeem-points', authenticate, orderController.redeemPoints);
 
-router.put('/admin/update-status/:orderId', authenticate, authorize('admin'), orderController.updateOrderStatus);
 module.exports = router;
